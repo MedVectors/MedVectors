6298b3fc402b50bc83cca7481184bec3f5b7a2b7
Aleksei Dudchenko <alexey.dudchenko@gmail.com>
2019-11-27 11:34:47 +0100

    gathering corpora into one file

---
+++ gathering.py
@@ -0,0 +1,19 @@
+from glob import glob
+
+# file
+GENERAL_PATH = 'C:/Users/admin/Documents/Folder/'
+
+FILE_TO_WRITE_PATH = "C:/Users/admin/Documents/MedVectors/Corpora.txt"
+file_to_write = open(FILE_TO_WRITE_PATH, "a+", encoding="utf8")
+
+i=0
+for filename in glob(GENERAL_PATH + '*.json'):
+    i+=1
+    print("" + i.__str__() + " " + filename)
+    file_to_read = open(filename, "r", encoding="utf8")
+    original_text = file_to_read.read()
+    file_to_write.write(original_text)
+
+
+
+
