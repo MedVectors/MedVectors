5ca8ecd84869dd3b384bbc75b5e166edb5b2e707
Aleksei Dudchenko <alexey.dudchenko@gmail.com>
2020-01-15 23:44:30 +0100

    dev. solve duplicates, concatenate and write in csv

--- solve_duplicates.py
+++ solve_duplicates.py
@@ -4,8 +4,6 @@ import read_txt as txt
 import pandas as pd
 pd.set_option('display.max_columns', 30)
 
-df = txt.get_dataframe_from_txt()
-
 @numba.jit
 def run(df, number_of_iterations, col_name):
     index = 0
@@ -18,46 +16,46 @@ def run(df, number_of_iterations, col_name):
     return df
 
 def get_unduplicated_lines(df):
-    # print("rows: " + df.shape[0].__str__())
+    print("rows: " + df.shape[0].__str__())
     df = df.reset_index()
     df = df.sort_values("id")
     df = run(df, df.shape[0] - 1, "t1")
-    # print("duplicated: " + df["id"].duplicated().sum().__str__())
+    print("duplicated: " + df["id"].duplicated().sum().__str__())
 
-    # print("rows: " + df.shape[0].__str__())
+    print("rows: " + df.shape[0].__str__())
     df = df.reset_index()
     df = df.drop("level_0", 1)
     df = df.sort_values("id")
     df = run(df, df.shape[0] - 1, "t2")
-    # print("duplicated: " + df["id"].duplicated().sum().__str__())
+    print("duplicated: " + df["id"].duplicated().sum().__str__())
 
-    # print("rows: " + df.shape[0].__str__())
+    print("rows: " + df.shape[0].__str__())
     df = df.reset_index()
     df = df.drop("level_0", 1)
     df = df.sort_values("id")
     df = run(df, df.shape[0] - 1, "t3")
-    # print("duplicated: " + df["id"].duplicated().sum().__str__())
+    print("duplicated: " + df["id"].duplicated().sum().__str__())
 
-    # print("rows: " + df.shape[0].__str__())
+    print("rows: " + df.shape[0].__str__())
     df = df.reset_index()
     df = df.drop("level_0", 1)
     df = df.sort_values("id")
     df = run(df, df.shape[0] - 1, "t4")
-    # print("duplicated: " + df["id"].duplicated().sum().__str__())
+    print("duplicated: " + df["id"].duplicated().sum().__str__())
 
-    # print("rows: " + df.shape[0].__str__())
+    print("rows: " + df.shape[0].__str__())
     df = df.reset_index()
     df = df.drop("level_0", 1)
     df = df.sort_values("id")
     df = run(df, df.shape[0] - 1, "t5")
-    # print("duplicated: " + df["id"].duplicated().sum().__str__())
+    print("duplicated: " + df["id"].duplicated().sum().__str__())
 
-    # print("rows: " + df.shape[0].__str__())
+    print("rows: " + df.shape[0].__str__())
     df = df.reset_index()
     df = df.drop("level_0", 1)
     df = df.sort_values("id")
     df = run(df, df.shape[0] - 1, "t6")
-    # print("duplicated: " + df["id"].duplicated().sum().__str__())
+    print("duplicated: " + df["id"].duplicated().sum().__str__())
 
     return df
 
