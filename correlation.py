2d75e456a5df0dc2305d9a48880f5cc6cda6e5b3
Aleksei Dudchenko <alexey.dudchenko@gmail.com>
2020-02-14 13:47:13 +0100

    plot correlations

--- correlation.py
+++ correlation.py
@@ -7,7 +7,7 @@ pd.set_option('display.max_rows', 10)
 pd.set_option('display.max_columns', None)
 
 # read
-file_name = "apgar3.csv"
+file_name = "files/only_apgar.csv"
 df = pd.read_csv(file_name)
 
 df = df.rename(columns={'Возраст_мать': 'mother_age'})
@@ -18,8 +18,9 @@ df = df.rename(columns={'аборты': 'abortion'})
 df = df.rename(columns={'кесарево': 'caesarean'})
 df = df.rename(columns={'ГБ_насл': 'GB'})
 df = df.rename(columns={'ИБС_насл': 'CHD'})
+df = df.rename(columns={'Апгар1': 'target'})
+df = df.fillna(0)
 
-print(df.columns)
 
 
 def plot_full_correlation_matrix():
@@ -69,10 +70,34 @@ def plot_target_correlations():
 
 def plot_target_correlation_short():
     correlated_df = df[["target", "szrp", "OAA", "abortion", "caesarean", "GB", "CHD"]]
+    ix = abs(correlated_df.corr()).sort_values('target', ascending=False).index
+    correlated_df = correlated_df.loc[:, ix]
     cor = correlated_df[correlated_df.columns[0:]].corr()['target'][1:]
     cor = abs(cor)
+    print(correlated_df.columns.values)
     cor.plot(kind='bar')
     plt.show()
+
+    # print(correlated_df.info())
+    # print(correlated_df.describe())
+
+
+
+def plot_target_correlation_all():
+    first_n_columns = 30
+    correlated_df = df[df.columns.values]
+    correlated_df = correlated_df.drop('Unnamed: 0', axis=1)
+    correlated_df = correlated_df.drop('level_0', axis=1)
+    correlated_df = correlated_df.drop('Unnamed: 0.1', axis=1)
+    correlated_df = correlated_df.drop('index', axis=1)
+    ix = abs(correlated_df.corr()).sort_values('target', ascending=False).index
+    correlated_df = correlated_df.loc[:, ix]
+    cor = correlated_df[correlated_df.columns[0:]].corr()['target'][1:first_n_columns]
+    cor = abs(cor)
+    cor.plot(kind='bar')
+    print(correlated_df.iloc[:, 1:first_n_columns].columns.values)
+    plt.show()
+#
 
 
 def get_short_df():
@@ -83,7 +108,7 @@ def get_short_df():
 # plot_weight_target_scatter()
 # plot_height_target_scatter()
 # plot_age_target_scatter()
-# plot_target_correlations()
+plot_target_correlation_short()
+plot_target_correlation_all()
+# plot_full_correlation_matrix()
 
-result_file_name = "short_dataset.csv"
-get_short_df().to_csv(result_file_name)
