77bb5cb8e0ba3cd6d8663f97b9bdfc214d329ef3
Aleksei Dudchenko <alexey.dudchenko@gmail.com>
2020-02-20 12:21:33 +0100

    file names in one separate file

---
+++ files/file_names.py
@@ -0,0 +1,14 @@
+
+txt_file_name = "C:/Users/admin/Downloads/Telegram Desktop/anamnesis-fixed-perinatal.txt"
+id_and_text_fn = "files/id_and_text.csv"
+utf8 = "utf8"
+
+xls_file_name = "C:/Users/admin/Downloads/Telegram Desktop/perinatal.xlsx"
+sheet_name = "L1"
+xls_to_csv_fn = "files/xls_to_csv.csv"
+
+concatenated_dataframe_file_name = "../files/result_no_duplicates.csv"
+apgar_file_name = "../files/only_apgar.csv"
+relevant_columns_file_name = "../files/short_dataset.csv"
+cleaned_text_file_name = "../files/cleaned_text.csv"
+result_file_name = "../files/final.csv"
